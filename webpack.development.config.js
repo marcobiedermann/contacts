7f16d941bc423ecc35e5ad49d80fe6058889a409
Marco Biedermann <marco.biedermann@gmx.de>
2018-07-31 21:27:12 +0200

    move file structure

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,4 +1,15 @@
+const path = require('path');
+
 module.exports = {
+  entry: {
+    main: [
+      path.resolve(__dirname, 'client/assets/js/index'),
+    ],
+  },
+  output: {
+    filename: 'assets/js/[name].js',
+    path: path.resolve(__dirname, 'server/public'),
+  },
   module: {
     rules: [
       {
