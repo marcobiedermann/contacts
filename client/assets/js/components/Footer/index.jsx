f89ebef8ea9a94d277583ab46dc7a4e8e2e82faf
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-02 00:05:43 +0200

    add Footer component

---
+++ client/assets/js/components/Footer/index.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Footer = ({ children }) => (
+  <footer className="footer">
+    {children}
+  </footer>
+);
+
+Footer.propTypes = {
+  children: PropTypes.node,
+};
+
+Footer.defaultProps = {
+  children: null,
+};
+
+export default Footer;
