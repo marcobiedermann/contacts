5ccda3a371e8b0bd2b1796f5ca530306697cfe96
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-02 00:05:09 +0200

    add Header component

---
+++ client/assets/js/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Header = ({ children }) => (
+  <header className="header">
+    {children}
+  </header>
+);
+
+Header.propTypes = {
+  children: PropTypes.node,
+};
+
+Header.defaultProps = {
+  children: null,
+};
+
+export default Header;
