3e0836446a77162f5f258fd7db4ad7ac9a556252
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-02 16:59:24 +0200

    add properties to contact

--- client/assets/js/components/Contact/index.jsx
+++ client/assets/js/components/Contact/index.jsx
@@ -2,16 +2,56 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Contact = ({ id }) => (
+const Contact = ({
+  address,
+  email,
+  id,
+  name,
+  phone,
+}) => (
   <div className="contact">
-    <Link to={`/contacts/${id}`}>
-      Contact
-    </Link>
+    <h1>
+      <Link to={`/contacts/${id}`}>
+        {name.firstName}
+        {' '}
+        {name.lastName}
+      </Link>
+    </h1>
+    <address>
+      {address.streetAddress}
+      <br />
+      {address.zipCode}
+      {' '}
+      {address.city}
+      <br />
+      {address.country}
+    </address>
+    <ul>
+      <li>
+        <a href={`mailto:${email}`}>
+          {email}
+        </a>
+      </li>
+      <li>
+        {phone}
+      </li>
+    </ul>
   </div>
 );
 
 Contact.propTypes = {
+  address: PropTypes.shape(),
+  email: PropTypes.string,
   id: PropTypes.string.isRequired,
+  name: PropTypes.shape(),
+  phone: PropTypes.string,
+};
+
+Contact.defaultProps = {
+  address: null,
+  email: '',
+  name: null,
+  phone: '',
 };
 
 export default Contact;
