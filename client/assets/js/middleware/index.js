c6fdf776cf7817e2a5dc094730a2f7e367c2b106
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-09 22:37:20 +0200

    add Redux DevTools Extension

--- client/assets/js/middleware/index.js
+++ client/assets/js/middleware/index.js
@@ -1,7 +1,12 @@
 import { routerMiddleware } from 'connected-react-router';
 import { applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import history from '../store/history';
 
-const middleware = applyMiddleware(routerMiddleware(history));
+const middleware = composeWithDevTools(
+  applyMiddleware(
+    routerMiddleware(history),
+  ),
+);
 
 export default middleware;
