b2215d19f141d96e046b9c9bef1af9125da3e003
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-19 23:39:33 +0200

    add Grid snapshot test

---
+++ client/assets/js/components/Grid/index.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Grid from '.';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Grid />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
