7bdebbad7592ae35695ec5ebee31605f196384bb
Marco Biedermann <marco.biedermann@gmx.de>
2018-08-19 23:42:54 +0200

    add Label snapshot test

---
+++ client/assets/js/components/Label/index.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Label from '.';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Label htmlFor="input" />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
