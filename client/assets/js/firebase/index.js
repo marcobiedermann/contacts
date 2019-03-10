27f8c26ca0ae50a0a2997b0784ebba1bd15e2676
Marco Biedermann <marco.biedermann@gmx.de>
2019-03-10 17:25:41 +0100

    fix: env
    load API keys via env file

--- client/assets/js/firebase/index.js
+++ client/assets/js/firebase/index.js
@@ -1,9 +1,12 @@
+/* eslint-disable prefer-destructuring */
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
+const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
+
 const fbConfig = {
-  apiKey: 'AIzaSyACCKsSpclO2wFOA4AwjNx4IPMjJAEsiOY',
+  apiKey: FIREBASE_API_KEY,
   authDomain: 'contacts-27c78.firebaseapp.com',
   databaseURL: 'https://contacts-27c78.firebaseio.com',
   projectId: 'contacts-27c78',
