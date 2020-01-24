1845794ffc5329a39a3b4765d016347ecdffe381
Marco Biedermann <marco.biedermann@gmx.de>
2020-01-24 13:47:45 +0100

    style: Prettier
    Format code using Prettier

--- client/assets/js/pages/ContactNewPage/index.jsx
+++ client/assets/js/pages/ContactNewPage/index.jsx
@@ -31,7 +31,4 @@ const mapDispatchToProps = dispatch =>
     dispatch,
   );
 
-export default connect(
-  null,
-  mapDispatchToProps,
-)(ContactNewPage);
+export default connect(null, mapDispatchToProps)(ContactNewPage);
