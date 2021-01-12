c0a0f09472eb6b5a35cc7db047f858f795e12252
Marco Biedermann <marco.biedermann@gmx.de>
2021-01-12 10:58:15 +0100

    refactor: Utilities
    Refactor utilities folder

--- src/assets/js/index.tsx
+++ src/assets/js/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Root from './components/Root';
-import './i18n';
-import './utiles/sentry';
+import './utils/i18n';
+import './utils/sentry';
 
 render(<Root />, document.getElementById('root'));
--- src/assets/js/utils/i18n.ts
+++ src/assets/js/utils/i18n.ts
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-xhr-backend';
 import { initReactI18next } from 'react-i18next';
-import { isDevelopment } from '../utiles/environment';
+import { isDevelopment } from './environment';
 
 i18n
   .use(Backend)
