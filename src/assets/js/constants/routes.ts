a2dafd47b9214ac6f9ab468599dbd32637f23c76
Marco Biedermann <marco.biedermann@gmx.de>
2021-01-15 14:56:06 +0100

    refactor: Page components
    Rename page component folders

--- src/assets/js/constants/routes.ts
+++ src/assets/js/constants/routes.ts
@@ -8,50 +8,50 @@ interface Route {
 }
 
 export const INDEX: Route = {
-  component: lazy(() => import('../pages/IndexPage')),
+  component: lazy(() => import('../pages/Index')),
   exact: true,
   name: 'pages:home',
   path: '/',
 };
 
 export const CONTACTS: Route = {
-  component: lazy(() => import('../pages/ContactsPage')),
+  component: lazy(() => import('../pages/Contacts')),
   name: 'pages:contacts',
   path: '/contacts',
 };
 
 export const CONTACT: Route = {
-  component: lazy(() => import('../pages/ContactPage')),
+  component: lazy(() => import('../pages/Contact')),
   name: 'pages:contact',
   path: '/contacts/:contactId',
 };
 
 export const CONTACT_EDIT: Route = {
-  component: lazy(() => import('../pages/ContactEditPage')),
+  component: lazy(() => import('../pages/ContactEdit')),
   name: 'pages:edit',
   path: '/contacts/:contactId/edit',
 };
 
 export const CONTACT_NEW: Route = {
-  component: lazy(() => import('../pages/ContactNewPage')),
+  component: lazy(() => import('../pages/ContactNew')),
   name: 'pages:new',
   path: '/contacts/new',
 };
 
 export const LOGIN: Route = {
-  component: lazy(() => import('../pages/LoginPage')),
+  component: lazy(() => import('../pages/Login')),
   name: 'pages:login',
   path: '/login',
 };
 
 export const LOGOUT: Route = {
-  component: lazy(() => import('../pages/LogoutPage')),
+  component: lazy(() => import('../pages/Logout')),
   name: 'pages:logout',
   path: '/logout',
 };
 
 export const REGISTER: Route = {
-  component: lazy(() => import('../pages/RegisterPage')),
+  component: lazy(() => import('../pages/Register')),
   name: 'pages:register',
   path: '/register',
 };
